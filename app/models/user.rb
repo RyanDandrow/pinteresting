f90d258daf4d5c291c9a75002073cf9ec1e588ac
Ryan Dandrow <RyanDandrow@gmail.com>
2015-02-17 14:43:16 -0600

    fixed bug in heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :pins
+  has_many :pins, dependent: :destroy
+  
 
   validates :name, presence: true
 end
