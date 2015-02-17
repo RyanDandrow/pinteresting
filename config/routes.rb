93d14bb74b86e068ae3e4df551c3ce260b51f258
Ryan Dandrow <RyanDandrow@gmail.com>
2015-02-16 21:24:20 -0600

    added S3, added name validations, email validations

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   devise_for :users
 root "pins#index"
 get "about" => "pages#about"
-get "new pin" => "pages#new_pin"
 
 
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141210193547) do
+ActiveRecord::Schema.define(version: 20150217020423) do
+
+  create_table "microposts", force: true do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "microposts", ["user_id", "created_at"], name: "index_microposts_on_user_id_and_created_at"
+  add_index "microposts", ["user_id"], name: "index_microposts_on_user_id"
 
   create_table "pins", force: true do |t|
     t.string   "description"
@@ -39,6 +49,7 @@ ActiveRecord::Schema.define(version: 20141210193547) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
